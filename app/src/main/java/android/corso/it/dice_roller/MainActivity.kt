bde9d5570e3bd8183eb3d9e1ea500faa49b662b7
giovanni vitiello <giovanni.vitiello10@gmail.com>
2019-10-09 12:51:50 +0200

    first commit dice roll

---
+++ app/src/main/java/android/corso/it/dice_roller/MainActivity.kt
@@ -0,0 +1,16 @@
+package android.corso.it.dice_roller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton : Button = findViewById(R.id.roll_button)
+        rollButton.text = "Let's Roll"
+
+    }
+}
