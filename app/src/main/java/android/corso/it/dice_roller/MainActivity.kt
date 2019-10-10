d1cc7b9c11d1762d4e0bc418b75805776337ac5e
giovanni vitiello <giovanni.vitiello10@gmail.com>
2019-10-10 09:15:50 +0200

    aggiunti file immagini

--- app/src/main/java/android/corso/it/dice_roller/MainActivity.kt
+++ app/src/main/java/android/corso/it/dice_roller/MainActivity.kt
@@ -3,6 +3,9 @@ package android.corso.it.dice_roller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,7 +13,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton : Button = findViewById(R.id.roll_button)
-        rollButton.text = "Let's Roll"
+        rollButton.setOnClickListener {
+            rollDice()
+
+        }
+        
+    }
+    private fun rollDice(){
+        val resultText : TextView = findViewById(R.id.result_text)
+        val randomInt = Random.nextInt(6) + 1
+        resultText.text = randomInt.toString()
 
     }
+
+    
 }
